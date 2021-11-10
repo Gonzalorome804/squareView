715be592059351427f5f8a45f065f4ecd61a128f
gonzalorome7 <93739735+gonzalorome7@users.noreply.github.com>
2021-11-10 14:39:50 -0300

    squareFinish

---
+++ squareView/squareView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  squareView
+//
+//  Created by Gonzalo Romero on 05/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
